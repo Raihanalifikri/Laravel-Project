8991f04398bb9d84425340dfc9df7f77400c52e6
Raihan Ali <m.raihanalifikri@gmail.com>
2024-02-16 10:50:08 +0700

    dir

--- app/Http/Controllers/adminController.php
+++ app/Http/Controllers/adminController.php
@@ -13,7 +13,7 @@ class adminController extends Controller
      */
     public function index()
     {
-        //
+        return view('admin.parent');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 
 // Route buat adminController Pake Resouce
 Route::resource('admin', adminController::class);
+
+
+
+
